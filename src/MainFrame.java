b9f54b99a925fb06de183ba0fba2b39a80559de2
105021094 <105021094@live.asia.edu.tw>
2017-10-24 13:03:50 +0800

    EX4

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class MainFrame extends JFrame {
     private int scrW = Toolkit.getDefaultToolkit().getScreenSize().width;
@@ -15,7 +16,14 @@ public class MainFrame extends JFrame {
     private JMenuItem jmiExit = new JMenuItem("Exit");
     private JMenuItem jmiLoto = new JMenuItem("Loto");
     private JDesktopPane jdp = new JDesktopPane();
+
     private JInternalFrame jInternalFrame = new JInternalFrame();
+    private Container jifCP;
+    private JPanel jpn = new JPanel(new GridLayout(1,6,5,5));
+    private JLabel jlbs[] = new JLabel[6];
+    private int data[] = new int[6];
+    private Random rnd = new Random(System.currentTimeMillis());
+
 
     public MainFrame(){
         initComp();
@@ -34,7 +42,17 @@ public class MainFrame extends JFrame {
         jmFile.add(jmiLoto);
 
         jInternalFrame.setBounds(0,0,300,80);
-
+        jifCP = jInternalFrame.getContentPane();
+        jifCP.setLayout(new BorderLayout(5,5));
+        jifCP.add(jpn,BorderLayout.CENTER);
+        for (int i = 0;i<6;i++){
+            jlbs[i] = new JLabel();
+            jlbs[i].setFont(new Font(null,Font.PLAIN,22));
+            jlbs[i].setBackground(Color.pink);
+            jlbs[i].setHorizontalAlignment(JLabel.CENTER);
+            jpn.add(jlbs[i]);
+        }
+        
         jmiLoto.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
